ed265ba0df42ea00214d12995dff4d607128d383
Anthony Cillo <arc85@pitt.edu>
2020-04-03 17:02:12 -0400

    Remove data for starter script

--- start_scrnaseq_workflow.sh
+++ start_scrnaseq_workflow.sh
@@ -1,4 +1,4 @@
 ##Run standard scRNAseq workflow locally
 ##TC March 6 2020
 
-R --vanilla -f auto_workflow_min_r_script.20.03.06.R --args integration=YES metadata=metadata_file.txt
+R --vanilla -f auto_workflow_min_r_script.R --args integration=YES metadata=metadata_file.txt

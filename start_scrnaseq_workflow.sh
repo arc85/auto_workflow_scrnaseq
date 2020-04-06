4f7cdb45ff3644ea7199c2be71fe7044de8f3a0e
Anthony Cillo <arc85@pitt.edu>
2020-04-06 11:30:54 -0400

    Update file name dependencies

--- start_scrnaseq_workflow.sh
+++ start_scrnaseq_workflow.sh
@@ -1,4 +1,4 @@
 ##Run standard scRNAseq workflow locally
 ##TC March 6 2020
 
-R --vanilla -f auto_workflow_min_r_script.R --args integration=YES metadata=metadata_file.txt
+R --vanilla -f auto_workflow_script.R --args integration=YES metadata=metadata_file.txt

e873d1e452655ca03d19a6dd464204b2a8de6424
Anthony Cillo <arc85@pitt.edu>
2020-04-07 16:28:13 -0400

    create ancillary_files folder and link rmd to datafiles in folder

--- start_scrnaseq_workflow.sh
+++ start_scrnaseq_workflow.sh
@@ -2,6 +2,6 @@
 ##TC March 6 2020
 
 R --vanilla -f auto_workflow_script.R --args integration=NO \
-  metadata=metadata_file.txt \
+  metadata=./ancillary_files/metadata_file.txt \
   unhashing=YES \
-  unhashing_file=cell_unhashing_identities.csv
+  unhashing_file=./ancillary_files/cell_unhashing_identities.csv

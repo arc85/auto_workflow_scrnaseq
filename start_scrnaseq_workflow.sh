f2ccb7d59f5bb5d5a5c83a7620561ce2b5e181c5
Anthony Cillo <arc85@pitt.edu>
2020-04-07 14:21:29 -0400

    Update --args to include cell hashing info

--- start_scrnaseq_workflow.sh
+++ start_scrnaseq_workflow.sh
@@ -1,4 +1,7 @@
 ##Run standard scRNAseq workflow locally
 ##TC March 6 2020
 
-R --vanilla -f auto_workflow_script.R --args integration=NO metadata=metadata_file.txt unhashing=cell_unhashing_identities.csv
+R --vanilla -f auto_workflow_script.R --args integration=NO \
+  metadata=metadata_file.txt \
+  unhashing=YES \
+  unhashing_file=cell_unhashing_identities.csv

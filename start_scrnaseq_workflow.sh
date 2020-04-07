ff32a8736d6e7fb25d1df2e884e9631e5e0b8217
Anthony Cillo <arc85@pitt.edu>
2020-04-07 14:10:17 -0400

    Add --arg for cell unhashing ids

--- start_scrnaseq_workflow.sh
+++ start_scrnaseq_workflow.sh
@@ -1,4 +1,4 @@
 ##Run standard scRNAseq workflow locally
 ##TC March 6 2020
 
-R --vanilla -f auto_workflow_script.R --args integration=NO metadata=metadata_file.txt
+R --vanilla -f auto_workflow_script.R --args integration=NO metadata=metadata_file.txt unhashing=cell_unhashing_identities.csv
